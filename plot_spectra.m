83261464b8f767bc9e49de6401dee76268972b15
Francesco Verdoja <francesco.verdoja@aalto.fi>
2018-06-27 16:43:18 +0300

    first version of plot_spectra.m

---
+++ plot_spectra.m
@@ -0,0 +1,64 @@
+%PLOT_SPECTRA Plots spectra for background and implanted anomalies
+clear;
+
+%% Load images
+load('salinas_impl_4.mat');
+X = o(75:200,:,:);
+
+load('salinas_impl_14.mat');
+X2 = o(75:200,:,:);
+
+load('salinas_impl_gt.mat');
+gt = g(75:200,:,:);
+
+%% Parameter
+% If N = 0, mean and std of all points in a region are plotted
+% If N > 0, the individual spectra for N random elements in a region are
+%           plotted instead
+N = 0;
+
+%% Get the elements
+sz = size(X);
+
+ai = find(gt);
+bi = find(1-gt);
+if N
+    ar = randi(length(ai), 1, N);
+    br = randi(length(bi), 1, N);
+    ai = ai(ar);
+    bi = bi(br);
+end
+
+X_ = reshape(X, sz(1)*sz(2), sz(3));
+X2_ = reshape(X2, sz(1)*sz(2), sz(3));
+
+A = X_(ai,:);
+A2 = X2_(ai,:);
+B = X_(bi,:);
+
+%% Plot the elements
+x = (0:sz(3)-1)';
+if N
+    figure; hold on;
+    for i = 1:N
+        plot(x,A(i,:),'b')
+        plot(x,A2(i,:),'g')
+        plot(x,B(i,:),'r')
+    end
+    hold off;
+else
+    mA = mean(A)';
+    mA2 = mean(A2)';
+    mB = mean(B)';
+    sA = std(A)';
+    sA2 = std(A2)';
+    sB = std(B)';
+    figure; hold on;
+    fill([x;flipud(x)],[mA-sA;flipud(mA+sA)],[.8 .8 1],'linestyle','none');
+    fill([x;flipud(x)],[mA2-sA2;flipud(mA2+sA2)],[.8 1 .8],'linestyle','none');
+    fill([x;flipud(x)],[mB-sB;flipud(mB+sB)],[1 .8 .8],'linestyle','none');
+    plot(x, mA, 'b');
+    plot(x, mA2, 'g');
+    plot(x, mB, 'r');
+    hold off;
+end
